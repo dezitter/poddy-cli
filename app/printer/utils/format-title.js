b818ef93c512db95d5c69babd5b68614768214bc
Laurent Dezitter <laurent.dezitter@gmail.com>
2017-01-28 16:13:10 -0500

    increase episode title width

--- app/printer/utils/format-title.js
+++ app/printer/utils/format-title.js
@@ -1,7 +1,7 @@
 import { ellipsize } from '../../utils/ellipsize';
 
 export function formatTitle(title) {
-    const titleWidth = 50;
+    const titleWidth = 80;
 
     return ellipsize(title, titleWidth)
         .padEnd(titleWidth);
